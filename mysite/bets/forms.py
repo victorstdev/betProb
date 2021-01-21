9dba4ea28721223f9909065ed89e609b2ca7c307
victorstdev <victorstdev@gmail.com>
2021-01-21 00:32:05 -0300

    add form cadastro

--- mysite/bets/forms.py
+++ mysite/bets/forms.py
@@ -1,8 +1,16 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 from .models import Bet
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class CadastrarBetForm(forms.ModelForm):
     
     class Meta:
         model = Bet
-        fields = '__all__'   
+        fields = '__all__'
+        widgets = {
+            'data': DateInput()
+        }
